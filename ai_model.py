3ad60eed16dea50633466dfb3d27ba88958b6c1a
Thayswetq123 <niederth@katharineum.de>
2026-03-11 23:49:17 +0100

    Create ai_model.py

---
+++ ai_model.py
@@ -0,0 +1,14 @@
+import numpy as np
+from PIL import ImageOps
+from tensorflow.keras.models import load_model
+
+model = load_model("models/keras_model")  # SavedModel Ordner
+labels = ["Schuh", "Hose", "Shirt"]  # Einfaches Beispiel, oder labels.txt laden
+
+def predict(image):
+    image = ImageOps.fit(image, (224,224))
+    arr = np.asarray(image).astype(np.float32)/127.5 - 1
+    data = np.expand_dims(arr, 0)
+    prediction = model.predict(data)
+    index = np.argmax(prediction)
+    return labels[index]
