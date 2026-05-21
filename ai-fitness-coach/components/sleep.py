ca90e35599b7d91ce99e0a53306585ab6bb95045
Thayswetq123 <niederth@katharineum.de>
2026-05-21 13:59:01 +0200

    Create sleep.py

---
+++ ai-fitness-coach/components/sleep.py
@@ -0,0 +1,9 @@
+def sleep_recommendation(goal):
+
+    if goal == "Muskelaufbau":
+        return "8-9 Stunden Schlaf empfohlen"
+
+    elif goal == "Fett verlieren":
+        return "7-8 Stunden Schlaf empfohlen"
+
+    return "Mindestens 8 Stunden Schlaf empfohlen"
